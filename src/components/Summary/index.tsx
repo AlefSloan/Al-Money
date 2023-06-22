553f68a4053a5f2941d65832b571c9a52f96560b
alefsloan <alef_sloan@hotmail.com>
2023-06-22 15:50:47 -0300

    feat: create Summary Component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,35 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { SummaryCard, SummaryContainer } from "./style";
+
+export function Summary() {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entrada</span>
+
+          <ArrowCircleUp size={26} color="#00b37e" />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saída</span>
+
+          <ArrowCircleDown size={26} color="#F75A68" />
+        </header>
+        <strong>R$ 1.259,00</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+
+          <CurrencyDollar size={26} color="#ffffff" />
+        </header>
+        <strong>R$ 16.141,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  );
+}
