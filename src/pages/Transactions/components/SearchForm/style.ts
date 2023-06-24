bb05091b1a5da155368a2280869f6c48d732b443
alefsloan <alef_sloan@hotmail.com>
2023-06-24 12:55:48 -0300

    feat: add form validation and submit on SearchForm

--- src/pages/Transactions/components/SearchForm/style.ts
+++ src/pages/Transactions/components/SearchForm/style.ts
@@ -27,12 +27,18 @@ export const SearchFormContent = styled.form`
     border-radius: 6px;
     color: ${(props) => props.theme['green-300']};
     font-weight: bold;
+    cursor: pointer;
 
-    &:hover {
+    &:not(:disabled):hover {
       background: ${(props) => props.theme['green-500']};
       border: 1px solid ${(props) => props.theme['green-500']};
       color: ${(props) => props.theme.white};
       transition: background-color 0.2s;
+    }
+
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
     }
   }
 `;
